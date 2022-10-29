91ba5266f31dd86c6507159736bd869a18c81722
Josh Bamford <joshbamford09@gmail.com>
2022-10-29 11:52:12 +0100

    refactor previous port and error when end reached

--- __tests__/Ships.test.js
+++ __tests__/Ships.test.js
@@ -19,14 +19,15 @@ describe('Ship', () => {
         expect(ship.currentPort).toBe(port)
     });
     it('can set sail', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
+        const dover = new Port('Dover');
+        const calais = new Port('Calais');
+        const itinerary = new Itinerary([dover, calais]);
         const ship = new Ship(itinerary);
 
         ship.setSail();
 
         expect(ship.currentPort).toBeFalsy();
-        expect(ship.previousPort).toBe(port);
+        expect(ship.previousPort).toBe(dover);
     });
     it('can dock at a different port', () => {
         const dover = new Port('Dover');
@@ -38,6 +39,17 @@ describe('Ship', () => {
         ship.dock();
 
         expect(ship.currentPort).toBe(calais);
+    });
+    it('can\'t sail further than its itinerary', () => {
+        const dover = new Port('Dover');
+        const calais = new Port('Calais');
+        const itinerary = new Itinerary([dover, calais]);
+        const ship = new Ship(itinerary);
+
+        ship.setSail();
+        ship.dock();
+
+        expect(() => ship.setSail()).toThrowError('End of itinerary reached');
     });
 });
 
