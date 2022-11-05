daed4979b834139882a1aa6c56a0adde31d03a5d
Josh Bamford <joshbamford09@gmail.com>
2022-11-05 12:43:11 +0000

    made ships.test DRY

--- __tests__/Ships.test.js
+++ __tests__/Ships.test.js
@@ -4,31 +4,35 @@ const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary.js');
 
 describe('Ship', () => {
-    it('can be instantiated', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
+    describe('with ports and itinerary', () => {
+        let ship;
+        let dover;
+        let calais;
+        let itinerary;
 
-        expect(ship).toBeInstanceOf(Object);
-    });
-    it('has a starting port', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
+        beforeEach(() => {
+            dover = new Port('Dover');
+            calais = new Port('Calais');
+            itinerary = new Itinerary([dover, calais]);
+            ship = new Ship(itinerary);
+        });
+        it('can be instantiated', () => {
+            expect(ship).toBeInstanceOf(Object);
+        });   
+        it('has a starting port', () => {
+            expect(ship.currentPort).toBe(dover)
+        });
+        it('can set sail', () => {
+            ship.setSail();
     
-        expect(ship.currentPort).toBe(port)
+            expect(ship.currentPort).toBeFalsy();
+            expect(dover.ships).not.toContain(ship);
+        });
+        it('gets added to port on instantation', () => {
+            expect(dover.ships).toContain(ship);
+        });
     });
-    it('can set sail', () => {
-        const dover = new Port('Dover');
-        const calais = new Port('Calais');
-        const itinerary = new Itinerary([dover, calais]);
-        const ship = new Ship(itinerary);
-
-        ship.setSail();
-
-        expect(ship.currentPort).toBeFalsy();
-        expect(dover.ships).not.toContain(ship);
-    });
+});
     it('can dock at a different port', () => {
         const dover = new Port('Dover');
         const calais = new Port('Calias');
@@ -59,5 +63,4 @@ describe('Ship', () => {
 
         expect(dover.ships).toContain(ship);
     });
-});
 
